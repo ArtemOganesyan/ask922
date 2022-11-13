f6b9371ad404c4b8c3e61e159d6f5b367398e99f
Bella Adamokova <belaadamokova@gmail.com>
2022-11-13 13:18:20 -0800

    new feature file added

---
+++ src/test/java/support/adamokovaXpathLib.java
@@ -0,0 +1,33 @@
+package support;
+
+public class adamokovaXpathLib {
+    public static String url = "http://ask-stage.portnov.com";
+    public static String emailField = "//input[@formcontrolname='email']";
+    public static String passwordField = "//input[@formcontrolname='password']";
+    public static String submitButton = "//*[contains(text(), 'Sign In')]";
+    public static String studentAccount = "//*[contains(text(), 'STUDENT')]";
+    public static String goToAssignments = "//span[contains(text(),'Go To My Assignments')]";
+    public static String assignmentsPage = "//body/ac-root[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/ac-student-assignments-page[1]/mat-card[1]";
+    public static String goToGrades = "//span[contains(text(),'Go To My Grades')]";
+    public static String gradesPage = "//body/ac-root[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/ac-student-grades-page[1]";
+    public static String goToSubmissions = "//body/ac-root[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/ac-home-page[1]/ac-home-teacher[1]/mat-card[1]/div[1]/a[1]/button[1]";
+    public static String submissionsPage = "//h4[contains(text(),'Submissions')]";
+    public static String goGoToAssignmentsTeacher = "//span[contains(text(),'Go To Assignments')]";
+    public static String assignmentsPageTeacher = "//h4[contains(text(),'List of Assignments')]";
+    public static String managementPage = "//body/ac-root[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/ac-user-management-page[1]";
+    public static String goToUsersManagementPage = "//span[contains(text(),'Go To Users Management')]";
+    public static String goToSettings = "//h5[contains(text(),'Settings')]";
+    public static String settingsPage = "//h4[contains(text(),'Settings')]";
+    public static String changeYourNameButton = "//span[contains(text(),'Change Your Name')]";
+    public static String dialogChangeName = "//div[@id='cdk-overlay-0']";
+    public static String NewNameField = "//input[@id='mat-input-2']";
+    public static String changeButton ="//body/div[2]/div[2]/div[1]/mat-dialog-container[1]/ac-modal-adjust-user[1]/form[1]/div[2]/button[2]";
+    public static String Pat_Kinger = "//td[contains(text(),'Pat Kinger')]";
+    public static String warningAlert = "//mat-error[@role='alert']";
+    public static String warNameFieldIsReq = "//mat-error[@id='mat-error-3']";
+    public static String characters = "//td[contains(text(),'qw rtyuiopasdfghjklzxcvbnmlkoiuhytgfredswqazsxcfdertyuijhgvbjhgfrtyuhgvcdfredsxhjhgtrfdecvhgtrfghjvbqwertyuiopasdfghjklzxcvbnmlkoiuhytgfredswqazsxcfdertyuijhgvbjhgfrtyuhgvcdfredsxhjhgtrfdecvhgtrfghjvbnbvcxzasdfghjkiuytgfredswedcvfgtrfgtyhjuikjhytgfredcvbhg')]";
+    public static String alphanumeric = "//td[contains(text(),'Alpha 1@>')]";
+
+
+}
+
