74f8478b61e723eb8384c8b1d13db5a064aa87cf
Alexey Kirchatykh <alexeywrite@gmail.com>
2022-11-13 18:58:17 -0800

    calabinDefinitions.java added to definitions

---
+++ src/test/java/definitions/calabinDefinitions.java
@@ -0,0 +1,13 @@
+package definitions;
+
+import cucumber.api.java.en.Given;
+
+import static support.TestContext.getDriver;
+
+public class calabinDefinitions {
+    @Given("VVC opens url {string}")
+    public void vvcOpensUrl(String arg0) {
+        getDriver().get("https://google.com");
+    }
+}
+
