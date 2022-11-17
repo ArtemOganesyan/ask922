d83ca8f06f7d02446a628cf88938580e9400a6d5
Kunduz Musina <kunduzmusina@iMac-Kunduz.local>
2022-11-17 09:55:20 -0600

    new vertion of test sets. No errors.

--- src/test/java/definitions/gudimovaDefinitions.java
+++ src/test/java/definitions/gudimovaDefinitions.java
@@ -4,9 +4,9 @@ import cucumber.api.java.en.Given;
 
 import static support.TestContext.getDriver;
 
-//public class gudimovaDefinitions {
-////    @Given("OAV open url {string}")
-//    public void oavOpenUrl(String arg0) {
-//        getDriver().get("https://google.com");
-//    }
-//}
+public class gudimovaDefinitions {
+//    @Given("OAV open url {string}")
+    public void oavOpenUrl(String arg0) {
+        getDriver().get("https://google.com");
+    }
+}
